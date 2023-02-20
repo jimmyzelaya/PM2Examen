a603f9c3ae1a48f29f425dff5c1e7514c5018064
Jimmy Zelaya <98270491+jimmyzelaya@users.noreply.github.com>
2023-02-19 20:18:03 -0600

    Agregar archivos de proyecto.

---
+++ PM2Examen/Controllers/dbsitios.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using PM2Examen.Models;
+using System.Threading.Tasks;
+
+namespace PM2Examen.Controllers
+{
+    public class dbsitios
+    {
+        readonly SQLiteAsyncConnection dbbase;
+
+        //Constructor de la clase  
+
+        public dbsitios(string dbpath)
+        {
+            //condision a la base de datos 
+            dbbase = new SQLiteAsyncConnection(dbpath);
+
+
+            //crearemos las tablas en la base de datos 
+            dbbase.CreateTableAsync<sitios>();
+        }
+        //hacemos la creacion del crud
+
+
+        public Task<int> sitioSave(sitios direcc)
+        {
+            if (direcc.id != 0)
+            {
+                return dbbase.UpdateAsync(direcc);//actualizamos
+            }
+            else
+            {
+                return dbbase.InsertAsync(direcc);//i si no esta lo inserta
+            }
+        }
+
+        //con read podemos leer informacion de la tabla 
+        //read
+        public Task<List<sitios>> ObtenerlistadoSitio()
+
+        {
+            return dbbase.Table<sitios>().ToListAsync();
+        }
+
+        //eliminar
+        public Task<int> eliminarsitio(sitios direcc)
+        {
+            return dbbase.DeleteAsync(direcc);
+
+        }
+        // Obtener Latitud de UbicacionesDB
+        public Task<sitios> ObtenerLongitud(string uLongitud)
+        {
+            return dbbase.Table<sitios>().Where(i => i.longitud == uLongitud).FirstOrDefaultAsync();
+        }
+
+        // Obtener Latitud de UbicacionesDB
+        public Task<sitios> ObtenerLatitud(string uLatitud)
+        {
+            return dbbase.Table<sitios>().Where(i => i.latitud == uLatitud).FirstOrDefaultAsync();
+        }
+
+        // Obtener Descripcion de UbicacionesDB
+        public Task<sitios> ObtenerDescripcion(String uDescripcion)
+        {
+            return dbbase.Table<sitios>().Where(i => i.descripcion == uDescripcion).FirstOrDefaultAsync();
+        }
+
+
+    }
+}
